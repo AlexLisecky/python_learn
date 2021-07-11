54e7345cc204f2948f3102efb0cbea9d3eb286a5
Alex <alexlisecky@gmail.com>
2021-07-11 20:32:35 +0300

    add_lesson_4

--- django/geekshop/basketapp/models.py
+++ django/geekshop/basketapp/models.py
@@ -2,11 +2,26 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+class BasketQuerySet(models.QuerySet):
+
+   def delete(self, *args, **kwargs):
+       for object in self:
+           object.product.quantity += object.quantity
+           object.product.save()
+       super(BasketQuerySet, self).delete(*args, **kwargs)
+
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
     
     
     def _get_product_cost(self):
@@ -32,3 +47,11 @@ class Basket(models.Model):
         return _totalcost
         
     total_cost = property(_get_total_cost)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
--- django/geekshop/ordersapp/models.py
+++ django/geekshop/ordersapp/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+class OrderItemQuerySet(models.QuerySet):
+
+   def delete(self, *args, **kwargs):
+       for object in self:
+           object.product.quantity += object.quantity
+           object.product.save()
+       super(OrderItemQuerySet, self).delete(*args, **kwargs)
 
 class Order(models.Model):
     FORMING = 'FM'
@@ -60,6 +67,7 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
+    objects = OrderItemQuerySet.as_manager()
     order = models.ForeignKey(Order,
                               related_name="orderitems",
                               on_delete=models.CASCADE)
@@ -71,3 +79,8 @@ class OrderItem(models.Model):
 
     def get_product_cost(self):
         return self.product.price * self.quantity
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
